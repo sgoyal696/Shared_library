61aae4d468324e6cc1ffeb49b8371a893c02d49e
Shubham Aggarwal <sgoyal696@gmail.com>
2023-08-26 07:49:41 +0000

    checkout groovy code written

---
+++ Variables/gitCheckout.groovy
@@ -0,0 +1,9 @@
+def call(Map stageParam){
+   
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name: stageParam.branch]],
+        userRemoteConfigs: [[url: stageParam.url]]
+    ])
+
+}
