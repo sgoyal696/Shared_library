205de83865b7ad87450db7574d4c04b0fdd42b58
Shubham Aggarwal <sgoyal696@gmail.com>
2023-08-31 08:14:37 +0000

    update code

--- vars/staticCodeAnalysis.groovy
+++ vars/staticCodeAnalysis.groovy
@@ -1,5 +1,5 @@
-def call(){
-    withSonarQubeEnv(credentialsId : "sonarqube-id") {
+def call(credentialsId){
+    withSonarQubeEnv(credentialsId : credentialsId) {
         sh "mvn clean package sonar:sonar"
     }
 
